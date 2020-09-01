1073e7bb48cc24b3cfaf91dac20522093eb234b4
Adam Elek <elekadam21@gmail.com>
2020-09-01 10:27:42 +0200

    App.js bugfix

--- src/App.js
+++ src/App.js
@@ -6,40 +6,17 @@ import Navbar from "./components/layout/Navbar";
 import WallpaperList from "./components/pages/WallpaperList";
 
 const App = () => {
-  const [wallpapers, setWallpapers] = useState(null);
-
-  useEffect(() => {
-    axios.get("https://wallhaven.cc/api/v1/search").then((res) => {
-      setWallpapers(res);
-    });
-  }, []);
-
-  let content = "Loading...";
-
-  if (wallpapers) {
-    console.log(wallpapers.data.data);
-    wallpapers.data.data.forEach(item => {
-      if (item.category === "anime"){
-        const index = wallpapers.data.data.indexOf(item);
-        if (index > -1) {
-          wallpapers.data.data.splice(index, 1);
-        }
-      }
-    })
-    content = (
-      <div>
-        {wallpapers.data.data.map((wallpaper) => (
-          <img
-            src={wallpaper.thumbs.small}
-            alt="Wallpaper"
-            key={wallpaper.thumbs.small}
-          />
-        ))}
-      </div>
-    );
-  }
-
-  return <div className="App">{content}</div>;
+  return (
+    <React.Fragment>
+      <Router>
+        <div className="App">
+          <Header />
+          <Navbar />
+          <Route path="/wallpapers" render={(props) => <WallpaperList />} />
+        </div>
+      </Router>
+    </React.Fragment>
+  );
 };
 
 export default App;

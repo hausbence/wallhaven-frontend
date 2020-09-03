ca17cd88f30855fab12e48ed47167d7e6edc7289
maryn1421 <zolnaimaryn1421@gmail.com>
2020-09-03 17:23:50 +0200

    added some design to resolution select box, removed this box from the main page, and made it visible to all of the pages which are using the wallpaperList component, and also refactored the tagpage, its now using the WallpaperList component

--- src/components/pages/MainPage/MainPage.js
+++ src/components/pages/MainPage/MainPage.js
@@ -1,47 +1,14 @@
-import React, {useState, useEffect} from "react";
-import {useHttp} from "../../../hooks/http";
-import {Link, BrowserRouter as Router, Route} from "react-router-dom";
-import loadingGif from "../../../loading2.gif";
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
 import WallpaperList from "../WallpaperList";
-import TagPage from "../TagPage/TagPage";
 import './mainPage.css';
 import '../../layout/Dropdown.css';
 
-
-
 const MainPage = () => {
-
-    const [value, setValue] = useState('something');
     const [url, setUrl] = useState('https://wallhaven.cc/api/v1/search?categories=100&purity=100&sorting=random&order=desc');
 
-    const handleSubmit = (event) => {
-        let result = "&resolutions=" + value;
-        setUrl('https://wallhaven.cc/api/v1/search?categories=100&purity=100&sorting=random&order=desc' + result);
-        console.log(url);
-        event.preventDefault();
-    }
-
-    const handleChange = () => {
-        let resolutionValue = document.getElementById("dropdown").value;
-        setValue(resolutionValue);
-    }
-
-    let mainContent = (
+    return (
         <div>
-            <div>
-                <div className="box">
-                    <form onSubmit={handleSubmit}>
-                        <label>
-                            <select id="dropdown" value={value} onChange={handleChange}>
-                                <option value="1920x1080">1920x1080</option>
-                                <option value="1680x1050">1680x1050</option>
-                                <option value="1280x720">1280x720</option>
-                            </select>
-                        </label>
-                        <input type="submit" value="Submit" />
-                    </form>
-                </div>
-            </div>
             <div className={"tag-container"}>
                 <Link to={"/tag/537"}><p>#League of Legends</p></Link>
                 <Link to={"/tag/65348"}><p>#4K</p></Link>
@@ -78,10 +45,7 @@ const MainPage = () => {
             </div>
         </div>
 
-    )
-
-
-    return mainContent;
+    );
 }
 
 export default MainPage;
--- src/components/pages/TagPage/TagPage.js
+++ src/components/pages/TagPage/TagPage.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
-import {Link, useParams} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
 import {useHttp} from "../../../hooks/http";
-import loadingGif from "../../../loading2.gif";
-
+import WallpaperList from "../WallpaperList";
 
 
 const TagPage = (props) => {
@@ -10,90 +9,37 @@ const TagPage = (props) => {
     const [pageByTag, setPageByTag] = useState(
         `https://wallhaven.cc/api/v1/search?categories=100&purity=100&q=+id:${id}`)
     const [wallpaperUrl] = useState(`https://wallhaven.cc/api/v1/search?categories=100&purity=100&q=+id:${id}`);
-    const [limit] = useState(24);
-    const [isloading2, fetchedTagData] = useHttp(`https://wallhaven.cc/api/v1/tag/${id}`,[]
-
+    const [isloading2, fetchedTagData] = useHttp(`https://wallhaven.cc/api/v1/tag/${id}`, []
     )
     const [tagData, setTagData] = useState([])
-    const [page, setPage] = useState(1);
-    const [wallpapers, setWallpapers] = useState([]);
-    const [isLoading, fetchedData] = useHttp(
-        wallpaperUrl + "&page=" + page.toString(),
-        [(wallpaperUrl, limit, page)]
-    );
 
-    useEffect(() => {
-        if (fetchedData) {
-            setWallpapers(fetchedData.data.data);
-        }
-    }, [fetchedData]);
 
     useEffect(() => {
         if (fetchedTagData) {
-            //console.log(fetchedTagData.data.data)
             setTagData(fetchedTagData.data.data)
         }
     }, [fetchedTagData])
 
-    let content = (
-        <div className={"loading-container"}>
-            <img src={loadingGif} alt={"loading"} />
-        </div>
-    );
+    let content;
 
-    let pageButtons = (
-        <div className={"button-container"}>
-            <button className={"btn"}
-                    onClick={() => {
-                        if (fetchedData.data.meta.current_page > 1) {
-                            setPage(page - 1);
-                        }
-                    }}
-            >
-                Previous
-            </button>
-            <button className={"btn"}
-                    onClick={() => {
-                        if (
-                            fetchedData.data.meta.current_page < fetchedData.data.meta.last_page
-                        ) {
-                            setPage(page + 1);
-                        }
-                    }}
-            >
-                Next
-            </button>
-        </div>
-    );
 
-    if (wallpapers && tagData) {
+    if (tagData) {
         content = (
-            <React.Fragment>
-                <div className={"tag-header"}><h1>{tagData.name}</h1></div>
-                <div className={"wallpaper-container"}>
-                    {wallpapers.slice(0, limit).map((wallpaper) => (
-                        <Link
-                            to={"/wallpaper/" + wallpaper.id}
-                            key={wallpaper.thumbs.small}
-                        >
-                            <img
-                                src={wallpaper.thumbs.small}
-                                alt="Wallpaper"
-                                key={wallpaper.thumbs.small}
-                            />
-                        </Link>
-                    ))}
-                    {!props.mainpage ? pageButtons : null}
-                </div>
-            </React.Fragment>
-        );
-    } else if (fetchedData && !wallpapers) {
+            <div>
+            <div className={"tag-header"}><h1>#{tagData.name}</h1></div>
+            <div>
+            <WallpaperList
+                url={wallpaperUrl}
+                limit="24"
+                page="1"
+            />
+            </div>
+            </div>)
+    } else  {
         content = <p>Could not fetch any data.</p>;
     }
     return content;
 }
-
-
 
 
 export default TagPage;

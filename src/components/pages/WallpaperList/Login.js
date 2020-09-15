1d46456da9fbd810f36a22259e5ffed812183200
hausbence <bence.hausknecht@gmail.com>
2020-09-15 18:41:57 +0200

    saving email & password in to cookies

--- src/components/pages/WallpaperList/Login.js
+++ src/components/pages/WallpaperList/Login.js
@@ -1,17 +1,24 @@
 import React, {useState} from "react";
 import {useForm} from "react-hook-form";
 import Axios from "axios";
+import { useCookies } from "react-cookie";
 
 
 const Login = () => {
     const { handleSubmit, register, errors } = useForm();
     const [login, setLogin] = useState(false)
+    const [cookies, setCookie] = useCookies(["email", "password"]);
     const onSubmit = (values) => {
         Axios.get(`http://localhost:8080/login/${values.email}/${values.password}`).then(r =>
             setLogin(r.data)
         )
         console.log(values);
+        if (login) {
+            setCookie("email", values.email )
+            setCookie("password", values.password)
+        }
     }
+
     let logged = "";
     if (login) {
         logged = "logged in";
@@ -23,6 +30,8 @@ const Login = () => {
     return (
         <React.Fragment>
             <h1>{logged}</h1>
+            <h2>{cookies.email}</h2>
+            <h2>{cookies.password}</h2>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <input
                     name="email"

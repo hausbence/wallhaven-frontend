12bb171113217bef20416d9dd7d3f29817541ffa
Adam Elek <elekadam21@gmail.com>
2020-09-16 12:58:18 +0200

    new options to navbar

--- src/App.js
+++ src/App.js
@@ -10,60 +10,55 @@ import TagPage from "./components/pages/TagPage/TagPage";
 import MainPage from "./components/pages/MainPage/MainPage";
 import Registration from "./components/pages/Registration";
 import Login from "./components/pages/WallpaperList/Login";
-import { CookiesProvider } from "react-cookie";
 
 const App = () => {
   return (
-    <React.Fragment>
-      <Router>
-        <Switch>
-          <React.Fragment>
-            <div className="App">
-              <Header />
-              <Navbar />
-              <Route exact path="/" component={MainPage} />
-              <Route
-                path="/random"
-                render={(props) => (
-                  <WallpaperList
-                    url="https://wallhaven.cc/api/v1/search?categories=100&purity=100&sorting=random&order=desc"
-                    limit="24"
-                    page="1"
-                  />
-                )}
-              />
-              <Route
-                path="/toplist"
-                render={(props) => (
-                  <WallpaperList
-                    url="https://wallhaven.cc/api/v1/search?categories=100&purity=100&sorting=toplist&order=desc&topRange=1y"
-                    limit="24"
-                    page="1"
-                  />
-                )}
-              />
-              <Route
-                path="/latest"
-                render={(props) => (
-                  <WallpaperList
-                    url="https://wallhaven.cc/api/v1/search?categories=100&purity=100"
-                    limit="24"
-                    page="1"
-                  />
-                )}
-              />
-              <Route exact path="/search/:searchTerm" component={Search} />
-              <Route exact path="/wallpaper/:id" component={SingleWallpaper} />
-              <Route exact path="/tag/:id" component={TagPage} />
-              <Route exact path="/registration" component={Registration} />
-              <CookiesProvider>
-                  <Route exact path="/login" component={Login} />
-              </CookiesProvider>
-            </div>
-          </React.Fragment>
-        </Switch>
-      </Router>
-    </React.Fragment>
+    <Router>
+      <Switch>
+        <React.Fragment>
+          <div className="App">
+            <Header />
+            <Navbar />
+            <Route exact path="/" component={MainPage} />
+            <Route
+              path="/random"
+              render={(props) => (
+                <WallpaperList
+                  url="https://wallhaven.cc/api/v1/search?categories=100&purity=100&sorting=random&order=desc"
+                  limit="24"
+                  page="1"
+                />
+              )}
+            />
+            <Route
+              path="/toplist"
+              render={(props) => (
+                <WallpaperList
+                  url="https://wallhaven.cc/api/v1/search?categories=100&purity=100&sorting=toplist&order=desc&topRange=1y"
+                  limit="24"
+                  page="1"
+                />
+              )}
+            />
+            <Route
+              path="/latest"
+              render={(props) => (
+                <WallpaperList
+                  url="https://wallhaven.cc/api/v1/search?categories=100&purity=100"
+                  limit="24"
+                  page="1"
+                />
+              )}
+            />
+            <Route exact path="/search/:searchTerm" component={Search} />
+            <Route exact path="/wallpaper/:id" component={SingleWallpaper} />
+            <Route exact path="/tag/:id" component={TagPage} />
+            <Route exact path="/registration" component={Registration} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </React.Fragment>
+      </Switch>
+    </Router>
   );
 };
 
--- src/components/pages/WallpaperList/Login.js
+++ src/components/pages/WallpaperList/Login.js
@@ -1,55 +1,54 @@
-import React, {useState} from "react";
-import {useForm} from "react-hook-form";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import Axios from "axios";
 import { useCookies } from "react-cookie";
 
-
 const Login = () => {
-    const { handleSubmit, register, errors } = useForm();
-    const [login, setLogin] = useState(false)
-    const [cookies, setCookie] = useCookies(["email", "password"]);
-    const onSubmit = (values) => {
-        Axios.get(`http://localhost:8080/login/${values.email}/${values.password}`).then(r =>
-            setLogin(r.data)
-        )
-        console.log(values);
-        if (login) {
-            setCookie("email", values.email, {path : "/"} )
-            setCookie("password", values.password, {path : "/"})
-        }
-    }
+  const { handleSubmit, register, errors } = useForm();
+  const [login, setLogin] = useState(false);
+  const [cookies, setCookie] = useCookies(["email", "password"]);
+  const onSubmit = (values) => {
+    Axios.get(
+      `http://localhost:8080/login/${values.email}/${values.password}`
+    ).then((r) => setLogin(r.data));
+    console.log(values);
     if (login) {
-        console.log(cookies);
+      setCookie("email", values.email, { path: "/" });
+      setCookie("password", values.password, { path: "/" });
     }
+  };
+  if (login) {
+    console.log(cookies);
+  }
 
-    return (
-        <React.Fragment>
-            <h2>{cookies.email}</h2>
-            <h2>{cookies.password}</h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input
-                    name="email"
-                    type="email"
-                    ref={register({
-                        required: true,
-                    })}
-                />
-                {errors.email && errors.email.message}
-                <input
-                    name="password"
-                    type="password"
-                    ref={register({
-                        required: true,
-                        minLength: 2,
-                        maxLength: 16,
-                    })}
-                />
-                {errors.password && errors.password.message}
+  return (
+    <React.Fragment>
+      <h2>{cookies.email}</h2>
+      <h2>{cookies.password}</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="email"
+          type="email"
+          ref={register({
+            required: true,
+          })}
+        />
+        {errors.email && errors.email.message}
+        <input
+          name="password"
+          type="password"
+          ref={register({
+            required: true,
+            minLength: 2,
+            maxLength: 16,
+          })}
+        />
+        {errors.password && errors.password.message}
 
-                <button type="submit">Submit</button>
-            </form>
-        </React.Fragment>
-    );
+        <button type="submit">Submit</button>
+      </form>
+    </React.Fragment>
+  );
 };
 
 export default Login;

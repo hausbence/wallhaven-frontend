f34a372540fab0e7b206861a2d8fbbbc938fa170
hausbence <bence.hausknecht@gmail.com>
2020-09-17 11:43:36 +0200

    listing friends in cards

--- src/components/pages/UserPage/Profile.js
+++ src/components/pages/UserPage/Profile.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from "react";
 import Axios from "axios";
 import {useCookies} from "react-cookie";
+import userlogo from '../../../resources/mini_default_user.png';
+import './Profile.css';
 
 const Profile = () => {
     const [friends, setFriends] = useState([]);
@@ -20,13 +22,18 @@ const Profile = () => {
     console.log(friends);
 
     return (
-        <div>
+        <div className="friend-container">
             <h1>Hey, {cookies.username}</h1>
             <h3>Here is a list of your friends: </h3>
             <React.Fragment>
-                {friends.map((friend, i) => (
-                    <p key={i}>{friend.name}</p>
-                ))}
+                <div>
+                    {friends.map((friend, i) => (
+                        <div className="friend-card">
+                            <p key={i}>{friend.name}</p>
+                            <img key={i} className="imageStyle" src={userlogo} alt="userlogo"/>
+                        </div>
+                        ))}
+                </div>
             </React.Fragment>
         </div>
     )

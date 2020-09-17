f98d8e7ffebeee6e5a839cf5aa58c2c94d79c9c1
Adam Elek <elekadam21@gmail.com>
2020-09-17 15:48:12 +0200

    fixed get issues on login page

--- src/components/pages/Registration.js
+++ src/components/pages/Registration.js
@@ -8,7 +8,7 @@ const Registration = () => {
   const { handleSubmit, register, errors } = useForm();
 
   const onSubmit = (values) => {
-      Axios.post("http://localhost:8080/register", {
+    Axios.post("http://localhost:8080/register", {
       name: values.username,
       password: values.password,
       email: values.email,
@@ -20,57 +20,55 @@ const Registration = () => {
         console.log(error);
       },
       history.push("/login")
-  );
-      console.log(values);
+    );
+    console.log(values);
   };
-
-  //   pattern: {
-  //     value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-  //     message: "invalid email address",
-
-  // validate: (value) => value !== "admin" || "Nice try!",
-  //   }
 
   let content = (
     <React.Fragment>
+      <div className="form">
         <h2 className="loginHeader">Registration</h2>
-        <div className="form">
-          <form onSubmit={handleSubmit(onSubmit)}>
-              <input className="input-style"
-              name="email"
-              type="email"
-              placeholder="Email"
-              ref={register({
-                required: true,
-              })}
-            />
-            {errors.email && errors.email.message}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            className="input-style"
+            name="email"
+            type="email"
+            placeholder="Email"
+            ref={register({
+              required: true,
+            })}
+          />
+          {errors.email && errors.email.message}
 
-            <input className="input-style"
-              name="username"
-              placeholder="Username"
-              ref={register({
-                required: true,
-                minLength: 2,
-                maxLength: 16,
-              })}
-            />
-            {errors.username && errors.username.message}
-            <input className="input-style"
-              name="password"
-              type="password"
-              placeholder="Password"
-              ref={register({
-                required: true,
-                minLength: 2,
-                maxLength: 16,
-              })}
-            />
-            {errors.password && errors.password.message}
+          <input
+            className="input-style"
+            name="username"
+            placeholder="Username"
+            ref={register({
+              required: true,
+              minLength: 2,
+              maxLength: 16,
+            })}
+          />
+          {errors.username && errors.username.message}
+          <input
+            className="input-style"
+            name="password"
+            type="password"
+            placeholder="Password"
+            ref={register({
+              required: true,
+              minLength: 2,
+              maxLength: 16,
+            })}
+          />
+          {errors.password && errors.password.message}
 
-            <button type="submit" className="button">Submit</button>
-          </form>
-        </div>
+          <button type="submit" className="button">
+            Submit
+          </button>
+        </form>
+      </div>
     </React.Fragment>
   );
   return content;

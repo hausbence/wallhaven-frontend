db0f2842025b8dc1fe4c696c41babc56751a43f4
Maryn Zolnai <zolnaimaryn1421@gmail.com>
2020-09-30 13:05:30 +0200

    removed unused variables from login.js

--- src/components/pages/WallpaperList/Login.js
+++ src/components/pages/WallpaperList/Login.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {useForm} from "react-hook-form";
 import Axios from "axios";
 import {useCookies} from "react-cookie";
@@ -6,19 +6,8 @@ import "./Login.css";
 import {useHistory} from "react-router-dom";
 
 const Login = () => {
-    const [cookies1, removeCookie] = useCookies([
-        "email",
-        "password",
-        "id",
-        "username",
-    ]);
-    const [reload, setReload] = useState(false)
-    const [id, setId] = useState(0);
-    const [user, setUser] = useState([])
-    const [username, setUsername] = useState("");
     const {handleSubmit, register, errors} = useForm();
     const history = useHistory();
-    const [login, setLogin] = useState(false);
     const [cookies, setCookie] = useCookies([
         "id",
         "email",
@@ -26,9 +15,6 @@ const Login = () => {
         "username",
     ]);
     let url = "http://localhost:8080";
-
-
-
     function checkLogin(values) {
         return new Promise(resolve => {
             Axios.get(url + "/login/" + values.email + "/" + values.password).then( r => {
@@ -72,44 +58,41 @@ const Login = () => {
     }
 
 
-       let content = <React.Fragment>
-            <div className="form">
-                <h2 className="loginHeader">Login</h2>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <input
-                        className="input-style"
-                        id={"email"}
-                        name="email"
-                        type="email"
-                        placeholder="Email"
-                        ref={register({
-                            required: true,
-                        })}
-                    />
-                    {errors.email && errors.email.message}
-                    <input
-                        className="input-style"
-                        id={"password"}
-                        name="password"
-                        type="password"
-                        placeholder="Password"
-                        ref={register({
-                            required: true,
-                            minLength: 2,
-                            maxLength: 16,
-                        })}
-                    />
-                    {errors.password && errors.password.message}
+    return <React.Fragment>
+        <div className="form">
+            <h2 className="loginHeader">Login</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    className="input-style"
+                    id={"email"}
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    ref={register({
+                        required: true,
+                    })}
+                />
+                {errors.email && errors.email.message}
+                <input
+                    className="input-style"
+                    id={"password"}
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    ref={register({
+                        required: true,
+                        minLength: 2,
+                        maxLength: 16,
+                    })}
+                />
+                {errors.password && errors.password.message}
 
-                    <button type="submit" className="button">
-                        Submit
-                    </button>
-                </form>
-            </div>
-        </React.Fragment>
-
-
-    return content
+                <button type="submit" className="button">
+                    Submit
+                </button>
+            </form>
+        </div>
+    </React.Fragment>
 };
 
 export default Login;

b5b28dc1a7eb5e082bde37eed0e8cd2c6afb5227
Adam Elek <elekadam21@gmail.com>
2020-10-01 14:54:03 +0200

    fixed most warnings and errors and updated profile frontend

--- src/components/pages/UserPage/Profile.js
+++ src/components/pages/UserPage/Profile.js
@@ -1,146 +1,163 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Axios from "axios";
-import {useCookies} from "react-cookie";
-import userlogo from '../../../resources/mini_default_user.png';
-import './Profile.css';
-import {Link} from "react-router-dom";
+import { useCookies } from "react-cookie";
+import userlogo from "../../../resources/mini_default_user.png";
+import "./Profile.css";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
-    const [friends, setFriends] = useState([]);
-    const [users, setUsers ] = useState([]);
-    const [uploaded, setUploaded ] = useState([]);
-    const [favouriteIDS, setFavouriteIDS] = useState([]);
-    const [favouriteIMGS, setfavouriteIMGS] = useState([]);
-    const [cookies, setCookie] = useCookies(["id","username","email","password"])
+  const [friends, setFriends] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [uploaded, setUploaded] = useState([]);
+  const [favouriteIDS, setFavouriteIDS] = useState([]);
+  const [cookies] = useCookies(["id", "username", "email", "password"]);
 
-
-   async function getFriends() {
-        await Axios.get(
-            `http://localhost:8080/friends/${cookies.id}`
-        )
-            .then((response) => {
-                setFriends(response.data);
-                return  Axios.get(`http://localhost:8080/profile/favourites/${cookies.id}`);
-            })
-            .then((response) => {
-                setFavouriteIDS(response.data);
-                return Axios.get(`http://localhost:8080/users/${cookies.id}`)
-            }).then(response => {
-                setUsers(response.data)
-                return Axios.get(`http://localhost:8080/uploaded/${cookies.id}`)
-            }).then(response => {
-                setUploaded(response.data)
-            })
-            .catch((error) => console.log(error.response));
-            }
-
-
-
-    useEffect(() => {
-        getFriends().then();
-       // getFavouriteIDS();
-       // getFavouriteIMGS();
-    }, [])
-
-
-    console.log(uploaded, "uploaded")
-
-    let friendSuggestions = <h5>Cant find any </h5>;
-    if (users.length > 0) {
-        friendSuggestions = <div className="friend-container card__container">{users.map((user, i ) => (
-            <div key={i} className="friend-card">
-                <p>{user.name}</p>
-                <img className="imageStyle" src={userlogo} alt="userlogo"/>
-                <button className="add-friend-button" onClick={() => (
-                    addFriend(user.id)
-                )}>Follow</button>
-            </div>
-        ))}</div>
-    }
-
-    const getSubstring = (wid) => {
-        return wid.substring(0,2)
-    }
-
-
-    let favs = <p>You dont have any favorites</p>
-    console.log(favouriteIDS)
-    if (favouriteIDS.length > 0) {
-        favs = <div className={"wallpaper-container"}>
-            {favouriteIDS.map((fav, j ) => (
-                <div key={j}>
-                    <Link to={"/wallpaper/" + fav.wallpaperId}><img className="wallpaper-block" src={`https://th.wallhaven.cc/small/${getSubstring(fav.wallpaperId)}/${fav.wallpaperId}.jpg`} alt=""/></Link>
-                </div>
-            ))}
-        </div>
-    }
-
-    let content = <div><h1>LOADING...</h1></div>
-
-    const addFriend = (id) => {
-        Axios.post(`http://localhost:8080/addFriend/${cookies.id}/${id}`, {}).then(
-            (response) => {
-                console.log(response);
-            },
-            (error) => {
-                console.log(error);
-            }
+  async function getFriends() {
+    await Axios.get(`http://localhost:8080/friends/${cookies.id}`)
+      .then((response) => {
+        setFriends(response.data);
+        return Axios.get(
+          `http://localhost:8080/profile/favourites/${cookies.id}`
         );
-        setTimeout(() => {
-            getFriends().then()
+      })
+      .then((response) => {
+        setFavouriteIDS(response.data);
+        return Axios.get(`http://localhost:8080/users/${cookies.id}`);
+      })
+      .then((response) => {
+        setUsers(response.data);
+        return Axios.get(`http://localhost:8080/uploaded/${cookies.id}`);
+      })
+      .then((response) => {
+        setUploaded(response.data);
+      })
+      .catch((error) => console.log(error.response));
+  }
 
-        }, 300)
-    }
+  useEffect(() => {
+    getFriends().then();
+    // getFavouriteIDS();
+    // getFavouriteIMGS();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    const uploadPic = {
-        maxHeight: '200px',
-        maxWidth: '350px',
-        height: 'auto',
-        width: 'auto',
-    }
+  const getSubstring = (wid) => {
+    return wid.substring(0, 2);
+  };
 
-    if (friends && favouriteIDS ) {
-        console.log(uploaded)
-        console.log(friends[0]?.friends,  "ADA")
-        content = <div className="friend-container">
-            <h1>Hey, {cookies.username}</h1>
-            <h3>Here is a list of who you follow: </h3>
-            <React.Fragment>
-                <div className="friend-container card__container">
+  let content = (
+    <div>
+      <h1>Loading...</h1>
+    </div>
+  );
 
-                    {friends.map((friend, i) => (
-                            <div key={i} className="friend-card">
-                                <p>{friend.name}</p>
-                                <Link to={`/friend/${friend.id}`}>
-                                    <img className="userlogo-style" src={userlogo} alt="userlogo"/>
-                                </Link>
-                            </div>
-                        ))
-                    }
-                </div>
-                <h1>Favorites: </h1>
-                {favs}
-                <h1>Your uploads: </h1>
-                <div>
-                    <div className="wallpaper-container">
-                        {uploaded.map((img, i) => (
-                            <div key={i}>
-                                <img className="wallpaper-block" style={uploadPic} src={img.link} alt="userlogo"/>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-                <div><h1>Friend suggestions:</h1>
-                    {friendSuggestions}
-                </div>
-            </React.Fragment>
+  let friendsContent = <p>You are not following anyone.</p>;
+  if (friends.length > 0) {
+    friendsContent = (
+      <div className="friend-container">
+        {friends.map((friend, i) => (
+          <div key={i} className="friend-card">
+            <p>{friend.name}</p>
+            <Link to={`/friend/${friend.id}`}>
+              <img className="userlogo-style" src={userlogo} alt="userlogo" />
+            </Link>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  let favs = <p>You don't have any favorites.</p>;
+  if (favouriteIDS.length > 0) {
+    favs = (
+      <div className={"profile-wallpaper-container"}>
+        {favouriteIDS.map((fav, j) => (
+          <div key={j}>
+            <Link to={"/wallpaper/" + fav.wallpaperId}>
+              <img
+                className="profile-wallpaper-block"
+                src={`https://th.wallhaven.cc/small/${getSubstring(
+                  fav.wallpaperId
+                )}/${fav.wallpaperId}.jpg`}
+                alt=""
+              />
+            </Link>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  let uploadContent = <p>You don't have any uploads.</p>;
+  if (uploaded.length > 0) {
+    uploadContent = (
+      <div className="profile-wallpaper-container">
+        {uploaded.map((img) => (
+          <img
+            className="profile-wallpaper-block"
+            src={img.link}
+            alt="userlogo"
+            key={img.link}
+          />
+        ))}
+      </div>
+    );
+  }
+
+  let friendSuggestions = <p>Can't find any. </p>;
+  if (users.length > 0) {
+    friendSuggestions = (
+      <div className="friend-container">
+        {users.map((user, i) => (
+          <div key={i} className="friend-card">
+            <p>{user.name}</p>
+            <img className="imageStyle" src={userlogo} alt="userlogo" />
+            <button
+              className="add-friend-button"
+              onClick={() => addFriend(user.id)}
+            >
+              Follow
+            </button>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  const addFriend = (id) => {
+    Axios.post(`http://localhost:8080/addFriend/${cookies.id}/${id}`, {}).then(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    setTimeout(() => {
+      getFriends().then();
+    }, 300);
+  };
+
+  if (friends && favouriteIDS) {
+    content = (
+      <div className="profile-container">
+        <div className={"settings-button"}>
+          <Link to="/settings">User settings</Link>
         </div>
-    }
+        <h1>Hey, {cookies.username}</h1>
+        <h3>Here is a list of who you follow: </h3>
+        {friendsContent}
+        <h1>Favorites: </h1>
+        {favs}
+        <h1>Your uploads: </h1>
+        {uploadContent}
+        <h1>Friend suggestions:</h1>
+        {friendSuggestions}
+      </div>
+    );
+  }
 
-    return (
-        content
-    )
-}
-
+  return content;
+};
 
 export default Profile;
--- src/components/pages/WallpaperList/Login.js
+++ src/components/pages/WallpaperList/Login.js
@@ -1,98 +1,92 @@
-import React, {useState} from "react";
-import {useForm} from "react-hook-form";
+import React from "react";
+import { useForm } from "react-hook-form";
 import Axios from "axios";
-import {useCookies} from "react-cookie";
+import { useCookies } from "react-cookie";
 import "./Login.css";
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
-    const {handleSubmit, register, errors} = useForm();
-    const history = useHistory();
-    const [cookies, setCookie] = useCookies([
-        "id",
-        "email",
-        "password",
-        "username",
-    ]);
-    let url = "http://localhost:8080";
-    function checkLogin(values) {
-        return new Promise(resolve => {
-            Axios.get(url + "/login/" + values.email + "/" + values.password).then( r => {
-                    resolve(r.data)
-                }
-            )
-        })
-    }
-
-
-    async function loginCheck(values) {
-        const result = await checkLogin(values)
-        console.log(result)
-        if (result) {
-            const user = await getUserData(values)
-            console.log(user)
-            setCookie("id", user.id, {path: "/"});
-            setCookie("email", user.email, {path: "/"});
-            setCookie("password", user.password, {path: "/"});
-            setCookie("username", user.name, {path: "/"});
-            history.push({
-                pathname: `/`,
-            });
+  const { handleSubmit, register, errors } = useForm();
+  const history = useHistory();
+  const [, setCookie] = useCookies(["id", "email", "password", "username"]);
+  let url = "http://localhost:8080";
+  function checkLogin(values) {
+    return new Promise((resolve) => {
+      Axios.get(url + "/login/" + values.email + "/" + values.password).then(
+        (r) => {
+          resolve(r.data);
         }
-        else {
-            alert("invalid username or password!!")
-        }
+      );
+    });
+  }
+
+  async function loginCheck(values) {
+    const result = await checkLogin(values);
+    console.log(result);
+    if (result) {
+      const user = await getUserData(values);
+      console.log(user);
+      setCookie("id", user.id, { path: "/" });
+      setCookie("email", user.email, { path: "/" });
+      setCookie("password", user.password, { path: "/" });
+      setCookie("username", user.name, { path: "/" });
+      history.push({
+        pathname: `/`,
+      });
+    } else {
+      alert("invalid username or password!!");
     }
+  }
 
+  function getUserData(values) {
+    return new Promise((resolve) => {
+      Axios.get(url + "/login/" + values.email).then((r) => {
+        resolve(r.data);
+      });
+    });
+  }
 
-    function getUserData(values) {
-        return new Promise(resolve => {
-            Axios.get(url + "/login/" + values.email).then(r => {
-                resolve(r.data)
-            })
-        })
-    }
+  const onSubmit = (values) => {
+    loginCheck(values).then();
+  };
 
-    const onSubmit = (values) => {
-        loginCheck(values).then();
-    }
+  return (
+    <React.Fragment>
+      <div className="form">
+        <h2 className="loginHeader">Login</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            className="input-style"
+            id={"email"}
+            name="email"
+            type="email"
+            placeholder="Email"
+            ref={register({
+              required: true,
+            })}
+          />
+          {errors.email && errors.email.message}
+          <input
+            className="input-style"
+            id={"password"}
+            name="password"
+            type="password"
+            placeholder="Password"
+            ref={register({
+              required: true,
+              minLength: 2,
+              maxLength: 16,
+            })}
+          />
+          {errors.password && errors.password.message}
 
-
-    return <React.Fragment>
-        <div className="form">
-            <h2 className="loginHeader">Login</h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input
-                    className="input-style"
-                    id={"email"}
-                    name="email"
-                    type="email"
-                    placeholder="Email"
-                    ref={register({
-                        required: true,
-                    })}
-                />
-                {errors.email && errors.email.message}
-                <input
-                    className="input-style"
-                    id={"password"}
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                    ref={register({
-                        required: true,
-                        minLength: 2,
-                        maxLength: 16,
-                    })}
-                />
-                {errors.password && errors.password.message}
-
-                <button type="submit" className="button">
-                    Submit
-                </button>
-            </form>
-        </div>
+          <button type="submit" className="button">
+            Submit
+          </button>
+        </form>
+      </div>
     </React.Fragment>
+  );
 };
 
 export default Login;
